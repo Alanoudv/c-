729295dbaa59e82511c021dffbbc7c12754a8909
Alanoud <alanoodabduallah110@gmail.com>
2024-08-01 18:18:23 +0300

    Add project files.

---
+++ ConsoleApp1/Casting.cs
@@ -0,0 +1,18 @@
+public class Casting {
+
+
+    public void Test(){
+        System.Console.WriteLine("Whats your name");
+        string name =Console.ReadLine();
+        
+        
+        
+        System.Console.WriteLine("whats your age");
+        int age = Convert.ToInt32(Console.ReadLine());
+        
+
+
+        System.Console.WriteLine("your name is " +name );
+        System.Console.WriteLine("yout age is " +age+"  years old");
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1;
+
+public class Program
+{
+    public Program()
+    {
+
+
+        static void main(string[] args)
+    
+        {
+            Casting c1 = new Casting();
+
+            c1.Test();
+        }
+    }
+}
